59cdd503210ef2ce5d78969af4a02c29ca15b3ce
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-02 21:13:12 +0300

    check text

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,13 @@ const getHeaderLi = document.querySelectorAll('li');
 
 getHeaderLi.forEach((li) => {
   const span = document.createElement('span');
+  const textContent = li.childNodes[0].textContent.trim();
 
-  span.textContent = li.firstChild.textContent.trim();
-  li.firstChild.replaceWith(span);
+  if (textContent.toUpperCase() !== textContent.toLowerCase()) {
+    return;
+  }
+  span.textContent = textContent;
+  li.childNodes[0].replaceWith(span);
 
   span.addEventListener('click', () => {
     const ul = li.querySelector('ul');
